4d91dd842bd97a6454b9d3035f0047b75f04c344
VanJackal <vanjackal@protonmail.com>
2021-12-01 11:52:32 -0600

    initial image testing

--- process.py
+++ process.py
@@ -1,2 +1,14 @@
+"""
+This file should contain the functionality to process a single image/frame and return it as an array of bytes (1 per line)
+"""
 import cv2
 
+path = 'Images/TEST.png'
+
+img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+print(img)
+print(img[0,0])
+
+cv2.imshow("AAAH",img)
+
+cv2.waitKey(0)
